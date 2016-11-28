ccfd6d170a8d1a3aec82b8171bbe823481ae25f7
Yumiko Kohkawa <sa136002@bbt.ohmae.ac.jp>
2016-11-28 11:37:49 +0000

    新規登録機能・ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'signup',  to: 'users#new'
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  #get 'sessions/new'
+
+  #get 'sample/index'
+
+  #get 'sessions/new'
+
+  #root to: 'static_pages#home'
+  #get 'signup',  to: 'users#new'
 
   resources :users
 
